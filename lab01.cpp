c1f5315752ef77316e09446de05a04742e85dde0
smobo <daniel.windsor@gmail.com>
2014-09-16 12:06:14 -0400

    Create lab01.cpp

---
+++ lab01.cpp
@@ -0,0 +1,16 @@
+// Daniel Windsor SN: 100344670
+// Lab 01, Hello world
+// 11/09/2014 
+#include <iostream>
+
+using namespace std; 
+int main(){
+	Hello there!
+	int hi[12] = {72, 101, 108, 108, 111, 32, 116, 104, 101, 114, 101, 33};	
+	for (int i=0; i > 10; i++){
+		cout << char() hi[i];
+	}
+	return 0;
+}
+
+
